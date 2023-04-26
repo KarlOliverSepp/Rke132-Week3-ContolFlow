922bc921ca625856011bf2238d7e086dab0da697
Karl <karl.oliver.sepp@tktk.ee>
2023-04-27 00:25:51 +0300

    Add week 3 code

---
+++ Week3IfSwitch/Week 3 test Switch/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine("Mis hinne sul on? (sisesta A, B, C, D, E või F)");
+string hinne = Console.ReadLine().ToUpper();
+
+switch (hinne)
+{
+    case "A":
+        Console.WriteLine("Suurepärane!");
+        break;
+    case "B":
+        Console.WriteLine("Väga hea!");
+        break;
+    case "C":
+        Console.WriteLine("Hea!");
+        break;
+    case "D":
+        Console.WriteLine("Rahuldav!");
+        break;
+    case "E":
+        Console.WriteLine("Kasin!");
+        break;
+    case "F":
+        Console.WriteLine("Puudulik!");
+        break;
+    default:
+        Console.WriteLine("Vale väärtus!");
+        break;
+}
