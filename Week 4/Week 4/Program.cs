548dce18a0af77fc3835ddfbfc590196dae5afd0
Karl <karl.oliver.sepp@tktk.ee>
2023-05-12 22:29:37 +0300

    add all the rest codes

---
+++ Week 4/Week 4/Program.cs
@@ -0,0 +1,11 @@
+﻿//For-LOOP
+//DRY-principle- Do not repeat
+
+int i = 0;
+
+for(i = 0; i < 5; i++)
+{
+    Console.WriteLine("we dont need no education.");
+}
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day.");
